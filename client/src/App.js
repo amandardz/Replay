264a563220732da27bce7252802e977d8c386c59
Ashley <ashleyaggie@aol.com>
2021-07-07 20:40:01 -0500

    Committing changes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Credentials from "./pages/Credentials";
+import Homepage from "./pages/Homepage";
+import Dashboard from "./pages/Dashboard";
+import Search from "./pages/Search";
 
 function App() {
-  return ( 
-  <Credentials />
-  )
+  return <> 
+  <BrowserRouter>
+    <div>
+      <Credentials />
+      <Navbar />
+      <Switch>
+        <Route exact path={["/", "/home"]} component={Homepage} />
+        <Route exact path="/login" component={Credentials} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/search" component={Search} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+  
+  </>
 }
 
 export default App;
