a13a485db0ca88d1738ef913ca43b09c7ded0150
Liv <lopez.olivia17@outlook.com>
2021-07-12 19:44:19 -0500

    adjusted user.js in routes folder

--- routes/user.js
+++ routes/user.js
@@ -35,40 +35,9 @@ router.get('/token', async (req, res) => {
     console.log(error);
   }
 }
-//   try{
-//   const token = await axios ({
-//           method: 'POST',
-//           url: 'https://accounts.spotify.com/api/token',
-//           data: 'grant_type=client_credentials',
-//           headers: {
-//             'Content-Type': 'application/x-www-form-urlencoded',
-//             'Authorization': 'Basic '+ Buffer.from(process.env.SPOTIFY_CLIENT_ID+':'+process.env.SPOTIFY_CLIENT_SECRET).toString('base64')
-// }})
-// res.status(200).json({success:true, result:token})
-// }
-//   catch(err) {
-//     console.log(err)
-//     res.status(400).end();
-//   }
 
  )
 
 
 module.exports = router;
 
-// import axios from "axios";
-// require('dotenv').config();
-
-// export default {
-//   getToken: function() {
-//     return axios('https://accounts.spotify.com/api/token', {
-//       method: 'POST',
-//       data: 'grant_type=client_credentials',
-//       headers: {
-//         'Content-Type': 'application/x-www-form-urlencoded',
-//         'Authorization': 'Basic '+ btoa(process.env.SPOTIFY_CLIENT_ID+':'+process.env.SPOTIFY_CLIENT_SECRET)
-//         // 'Authorization': 'Basic '+ btoa('94ada71ea84d4da7bc17bb5d06d5b255'+':'+'eb0134cd97544880b1ac9ba09da3537d')
-//       }
-//     });
-//   }
-// }
