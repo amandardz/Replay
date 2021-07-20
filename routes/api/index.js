001cc569b910aa42d9a5b004fc77f6f6e2235970
Liv <lopez.olivia17@outlook.com>
2021-07-20 17:10:12 -0500

    added routes for youtube API

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const tokenRoute = require('./token');
+const youtubeRoute = require('./youtube')
 
 router.use('/user', userRoutes);
 router.use('/user/token', tokenRoute);
+router.use('/youtube', youtubeRoute);
 
 module.exports = router;
