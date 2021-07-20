260c0e0b7c2ade081b6bf227c581099cc5c4e0c1
Liv <lopez.olivia17@outlook.com>
2021-07-20 17:17:54 -0500

    merged files and adjusted search function in search.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const tokenRoute = require('./token');
-<<<<<<< HEAD
+
 const youtubeRoute = require('./youtube')
-=======
+
 const playlistRoutes = require('./playlistRoutes');
->>>>>>> 21018f35bee35e639b6138f8b52f8c301ba72984
+
 
 router.use('/user', userRoutes);
 router.use('/playlist', playlistRoutes);
