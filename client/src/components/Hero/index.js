c7521e4c0ed756c3e35071377a7faad404cbb13d
amandardz <amandardz15@gmail.com>
2021-07-21 11:23:00 -0500

    modiyfing styles for homepage

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -8,7 +8,7 @@ function Hero() {
 
   return(
     <div id='hero' className='heroBackground d-flex justify-content-center align-items-center'>
-      <div className='d-flex flex-column align-items-center'>
+      <div className='d-flex flex-column justify-content-center align-items-center'>
         <img src='/images/replay-logo2.png' alt='logo'/>
         <span id='slogan' className='text-center text-wrap'>Create playlists for your favorite songs.</span>
         <button type="button" className="btn btn-outline-primary mt-3" onClick={() => {history.replace('/login')}}>Login/Signup</button>
