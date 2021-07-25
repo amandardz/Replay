42ba411381b644135c1c644bd900471ae1edbefd
Liv <lopez.olivia17@outlook.com>
2021-07-24 19:48:08 -0500

    added back and next button to musicplayer / playlist

--- client/src/components/MusicPlayer/index.js
+++ client/src/components/MusicPlayer/index.js
@@ -3,23 +3,45 @@ import YouTube from 'react-youtube';
 
 class MusicPlayer extends React.Component {
   state = {
-    currentIndex: 0,
-  }
+    currentIndex: 0
+  };
+
+  handleNextBtn = () => {
+    this.setState({ currentIndex: this.state.currentIndex + 1 });
+  };
+
+  handleBackBtn = () => {
+    this.setState({ currentIndex: this.state.currentIndex - 1 });
+  };
 
   render() {
     const opts = {
-      height: '390',
-      width: '390',
+      height: '270',
+      width: '480',
       playerVars: {
         autoplay: 1,
         mute: 1
-      },
+      }
     };
-    console.log(this.props.videoLinks)
-    return <YouTube key={this.state.currentIndex} videoId={this.props.videoLinks[this.state.currentIndex]} opts={opts} onReady={this._onReady} onEnd={ () => {
-      this.setState({currentIndex: this.state.currentIndex + 1})
-    }
-    } />;
+    //console.log(this.props.videoLinks)
+    return( 
+      <>
+      <YouTube  
+        key={this.state.currentIndex} 
+        videoId={this.props.videoLinks[this.state.currentIndex]} 
+        opts={opts} 
+        onEnd={ () => {this.setState({currentIndex: this.state.currentIndex + 1})}} 
+      />
+    <div>
+    <button className="btn btn-danger" onClick={this.handleBackBtn}>
+            back
+          </button>
+        <button className="btn btn-primary" onClick={this.handleNextBtn}>
+            next
+          </button>
+    </div>
+    </>
+    )
   }
 }
 
